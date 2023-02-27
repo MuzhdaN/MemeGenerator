5cb8338ea57d7f11af71f2e0b036ed10a7ad74f3
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-02-27 21:50:21 +0600

    Added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // import './style.css'
 
+import Header from "./components/Header"
+
 function App() {
 
   return (
     <div>
-      <h1>hellp</h1>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export default function Header() {
+    return(
+        <div className='header'>
+            <img 
+                src='/public/vite.svg'
+                className='logo' 
+            />
+            <h3 className='header-title'>Meme Generator</h3>
+            <h4 className='header-subheading'>React course - project 4</h4>
+        </div>
+    )
+}
