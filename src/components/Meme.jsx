4a43f99338088914b0602d4af9563313f3b143e4
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-03-01 00:12:46 +0600

    Added the text for the meme

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 
 
 export default function Meme() {
+
+    const [meme, setMeme] = React.useState({
+        topTex: "", 
+        bottomText: "", 
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+
+    // writing/addding the text from the form (input)
+    function textChanged(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme, 
+                [name]:value
+            }
+        })
+    }
+
     return(
         <main>
             <form className='form'>
@@ -9,15 +27,24 @@ export default function Meme() {
                     type="text"
                     placeholder='Top Text'
                     className='form-input'
+                    value={meme.topText}
+                    name="topText"
+                    onChange={textChanged}
                 />
                 <input
                     type="text"
                     placeholder='Bottom Text'
                     className='form-input'
+                    value={meme.bottomText}
+                    name="bottomText"
+                    onChange={textChanged}
                 />
 
                 <button className='form-btn'>Change the meme image 🖼</button>
+
             </form>
+            <h3>{meme.topText}</h3>
+            <h3>{meme.bottomText}</h3>
         </main>
     )
 } 
